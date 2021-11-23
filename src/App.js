2195be0eded219a5938de9a9a7844b7977b24c23
vbarsott <vaplacencio@hotmail.com>
2021-11-22 21:39:18 -0500

    Created styles page for sass themes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './styles/css/styles.css';
 import { useState } from 'react';
-import BgInput from './components/BgInput';
 import ColourInput from './components/ColourInput';
 import FontSizeInput from './components/FontSizeInput';
 
 const App = () => {
   const [theme, setTheme] = useState({
-    size: 'md',
-    background: 'light',
-    color: 'green',
+    size: '',
+    color: '',
   });
 
-  const { size, background, color } = theme;
+  const { size, color } = theme;
 
   const onChange = (e) => {
     const { name, value } = e.target;
@@ -22,63 +20,19 @@ const App = () => {
     <>
       <div
         data-theme-size={size}
-        data-theme-background={background}
         data-theme-color={color}
         className='size background color'>
-        <header className='d-flex justify-content-between p-4'>
-          <h1 className='color-primary'>Theme Switcher</h1>
-          <form onChange={onChange}>
-            <h2 className='color-primary text-align-end'>
-              Change Background Color
-            </h2>
-            <div className='d-flex align-items-center py-2'>
-              <BgInput
-                backgroundColor='#f2f2f2'
-                color='#000'
-                value='light'
-                label='Light'
-                defaultChecked
-              />
-              <BgInput
-                backgroundColor='#666'
-                color='#f2f2f2'
-                value='grey'
-                label='Grey'
-              />
-              <BgInput
-                backgroundColor='#000'
-                color='#f2f2f2'
-                value='dark'
-                label='Dark'
-              />
-            </div>
-          </form>
-        </header>
-
-        <main className='p-4'>
-          <form onChange={onChange}>
-            <div className='theme-color'>
-              <h2 className='color-primary'>Change Theme Colors</h2>
-              <div className='w-50 d-flex justify-content-start align-items-center py-2'>
-                <ColourInput
-                  label='Blue'
-                  value='blue'
-                  color='#1da1f2'
-                  defaultChecked
-                />
-                <ColourInput label='Yellow' value='yellow' color='#ffad1f' />
-                <ColourInput label='Red' value='red' color='#e0255e' />
-                <ColourInput label='Purple' value='purple' color='#794bc3' />
-                <ColourInput label='Orange' value='orange' color='#f45e22' />
-                <ColourInput label='Green' value='green' color='#48d1cc' />
-              </div>
-            </div>
-            <div className='theme-size mt-4'>
-              <h2 className='color-primary'>Change Theme Sizes</h2>
-              <div className='w-50 d-flex justify-content-between align-items-center py-2'>
+        <header className='p-4'>
+          <form
+            onChange={onChange}
+            className='d-flex justify-content-between align-items-center'>
+            <h1 className='h1-stroke primary'>Theme Switcher</h1>
+            <div className='theme-sizes background'>
+              <h2 className='h2-stroke primary'>Change Theme Sizes</h2>
+              <div className='d-flex justify-content-between align-items-center py-2'>
                 <label
                   htmlFor='xs'
-                  className='color-primary'
+                  color='color'
                   style={{ fontSize: '0.8rem' }}>
                   <b>Aa</b>
                 </label>
@@ -86,39 +40,182 @@ const App = () => {
                   label='xs'
                   value='xs'
                   size='0.8rem'
-                  color={color}
+                  color='color'
                 />
                 <FontSizeInput
                   label='sm'
                   value='sm'
                   size='0.9rem'
-                  color={color}
+                  color='color'
                 />
                 <FontSizeInput
                   label='md'
                   value='md'
                   size='1rem'
-                  color='color-primary'
+                  color='color'
                   defaultChecked
                 />
                 <FontSizeInput
                   label='lg'
                   value='lg'
                   size='1.1rem'
-                  color='color-primary'
+                  color='color'
                 />
                 <FontSizeInput
                   label='xl'
                   value='xl'
                   size='1.2rem'
-                  color='color-primary'
+                  color='color'
                 />
                 <label
                   htmlFor='xl'
-                  className='color-primary'
+                  color='color'
                   style={{ fontSize: '1.2rem' }}>
                   <b>Aa</b>
                 </label>
+              </div>
+            </div>
+          </form>
+        </header>
+
+        <main>
+          <form onChange={onChange}>
+            <div className='theme-colors bg-theme p-4'>
+              <h2 className='h2-stroke  primary mb-2'>Change Theme Colors</h2>
+              <div className='w-50 d-flex justify-content-start align-items-center py-2'>
+                <ColourInput
+                  label='Light'
+                  value='light'
+                  backgroundColor='#f2f2f2'
+                  color='#333'
+                  defaultChecked
+                />
+                <ColourInput
+                  label='Grey'
+                  value='grey'
+                  backgroundColor='#777'
+                  color='#f2f2f2'
+                />
+                <ColourInput
+                  label='Dark'
+                  value='dark'
+                  backgroundColor='#333'
+                  color='#f2f2f2'
+                />
+                <ColourInput
+                  label='Floral'
+                  value='floral'
+                  backgroundColor='#e8d9d4'
+                  color='#333'
+                />
+                <ColourInput
+                  label='Blue'
+                  value='blue'
+                  backgroundColor='#b8cad1'
+                  color='#333'
+                />
+                <ColourInput
+                  label='Yellow'
+                  value='yellow'
+                  backgroundColor='#f7f4e0'
+                  color='#333'
+                />
+                <ColourInput
+                  label='Green'
+                  value='green'
+                  backgroundColor='#d8dace'
+                  color='#333'
+                />
+              </div>
+
+              <div className='mt-2 fw-bold fs-4'>
+                <h2 className='h2-stroke primary mx-2 '>Texts</h2>
+                <p className='color mx-2 my-1'>Text color</p>
+                <p className='primary mx-2 my-1'>Text primary</p>
+                <p className='secondary mx-2 my-1'>Text secondary</p>
+                <p className='accent mx-2 my-1'>Text accent</p>
+              </div>
+              <div className='mt-2 fw-bold fs-4'>
+                <h2 className='h2-stroke primary mx-2 '>Headings</h2>
+                <div className='d-flex align-items-end'>
+                  <h1 className='h1 primary mx-2 '>H1</h1>
+                  <h2 className='h2 primary mx-2 '>H2</h2>
+                  <h3 className='h3 primary mx-2 '>H3</h3>
+                  <h4 className='h4 primary mx-2 '>H4</h4>
+                  <h5 className='h5 primary mx-2 '>H5</h5>
+                  <h6 className='h6 primary mx-2 '>H6</h6>
+                </div>
+                <div className='d-flex align-items-end'>
+                  <h1 className='h1 secondary mx-2 '>H1</h1>
+                  <h2 className='h2 secondary mx-2 '>H2</h2>
+                  <h3 className='h3 secondary mx-2 '>H3</h3>
+                  <h4 className='h4 secondary mx-2 '>H4</h4>
+                  <h5 className='h5 secondary mx-2 '>H5</h5>
+                  <h6 className='h6 secondary mx-2 '>H6</h6>
+                </div>
+                <div className='d-flex align-items-end'>
+                  <h1 className='h1-styled primary mx-2 '>H1</h1>
+                  <h2 className='h2-styled primary mx-2 '>H2</h2>
+                  <h3 className='h3-styled primary mx-2 '>H3</h3>
+                  <h4 className='h4-styled primary mx-2 '>H4</h4>
+                  <h5 className='h5-styled primary mx-2 '>H5</h5>
+                  <h6 className='h6-styled primary mx-2 '>H6</h6>
+                </div>
+                <div className='d-flex align-items-end'>
+                  <h1 className='h1-styled secondary mx-2 '>H1</h1>
+                  <h2 className='h2-styled secondary mx-2 '>H2</h2>
+                  <h3 className='h3-styled secondary mx-2 '>H3</h3>
+                  <h4 className='h4-styled secondary mx-2 '>H4</h4>
+                  <h5 className='h5-styled secondary mx-2 '>H5</h5>
+                  <h6 className='h6-styled secondary mx-2 '>H6</h6>
+                </div>
+                <div className='d-flex align-items-end'>
+                  <h1 className='h1-stroke primary mx-2 '>H1 stroke</h1>
+                  <h2 className='h2-stroke primary mx-2 '>H2 stroke</h2>
+                  <h3 className='h3-stroke primary mx-2 '>H3 stroke</h3>
+                  <h4 className='h4-stroke primary mx-2 '>H4 stroke</h4>
+                  <h5 className='h5-stroke primary mx-2 '>H5 stroke</h5>
+                  <h6 className='h6-stroke primary mx-2 '>H6 stroke</h6>
+                </div>
+                <div className='d-flex align-items-end'>
+                  <h1 className='h1-stroke secondary mx-2 '>H1 stroke</h1>
+                  <h2 className='h2-stroke secondary mx-2 '>H2 stroke</h2>
+                  <h3 className='h3-stroke secondary mx-2 '>H3 stroke</h3>
+                  <h4 className='h4-stroke secondary mx-2 '>H4 stroke</h4>
+                  <h5 className='h5-stroke secondary mx-2 '>H5 stroke</h5>
+                  <h6 className='h6-stroke secondary mx-2 '>H6 stroke</h6>
+                </div>
+              </div>
+            </div>
+            <div className='theme-buttons p-4'>
+              <h2 className='h2-stroke primary'>Theme Buttons</h2>
+              <div className='d-flex p-2'>
+                <button className='theme-btn-light my-2 me-4' type='submit'>
+                  Button Light
+                </button>
+                <button className='theme-btn-dark my-2 me-4' type='submit'>
+                  Button Dark
+                </button>
+                <button className='btn-accent-light my-2 me-4' type='submit'>
+                  Accent Light
+                </button>
+                <button className='btn-accent-dark my-2' type='submit'>
+                  Accent Dark
+                </button>
+              </div>
+              <div className='bg-theme d-flex p-2'>
+                <button className='theme-btn-light my-2 me-4' type='submit'>
+                  Button Light
+                </button>
+                <button className='theme-btn-dark my-2 me-4' type='submit'>
+                  Button Dark
+                </button>
+                <button className='btn-accent-light my-2 me-4' type='submit'>
+                  Accent Light
+                </button>
+                <button className='btn-accent-dark my-2' type='submit'>
+                  Accent Dark
+                </button>
               </div>
             </div>
           </form>
--- src/components/ColourInput.js
+++ src/components/ColourInput.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-const ColourInput = ({ color, label, value, defaultChecked = false }) => {
+const ColourInput = ({
+  backgroundColor,
+  color,
+  label,
+  value,
+  defaultChecked = false,
+}) => {
   return (
     <>
       <div
         className='d-flex flex-column align-items-center me-2 p-3'
-        style={{ color, borderRadius: '50%', border: `1px solid ${color}` }}>
+        style={{
+          backgroundColor,
+          color,
+          borderRadius: '50%',
+          border: `1px solid ${color}`,
+        }}>
         <input
           type='radio'
           name='color'
